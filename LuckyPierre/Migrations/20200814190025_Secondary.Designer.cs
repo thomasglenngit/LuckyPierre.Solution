dbf7b8536336021627f1fa20ee18bb1517335817
Thomas Glenn <thomasgrantglenn@mac.com>
2020-08-14 13:52:31 -0600

    add Home Index view.

--- LuckyPierre/Migrations/20200814190025_Secondary.Designer.cs
+++ LuckyPierre/Migrations/20200814190025_Secondary.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using LuckyPierre.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LuckyPierre.Migrations
 {
     [DbContext(typeof(LuckyPierreContext))]
-    partial class LuckyPierreContextModelSnapshot : ModelSnapshot
+    [Migration("20200814190025_Secondary")]
+    partial class Secondary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
